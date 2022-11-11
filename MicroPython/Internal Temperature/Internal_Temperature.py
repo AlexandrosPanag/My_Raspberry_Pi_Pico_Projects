2726323d2256020a35079ce0126dd2a62548ee3c
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2022-11-11 13:36:01 +0200

    Update Internal_Temperature.py

--- MicroPython/Internal Temperature/Internal_Temperature.py
+++ MicroPython/Internal Temperature/Internal_Temperature.py
@@ -1,11 +1,13 @@
-import machine
-import utime
+# This code was edited by Alexandros Panagiotakopoulos
+
+import machine #import machine library
+import utime #import utime
+  
+sensor_temp = machine.ADC(4) #read the internal temperature
+conversion_factor = 3.3 / (65535) #convert the temperature
  
-sensor_temp = machine.ADC(4)
-conversion_factor = 3.3 / (65535)
- 
-while True:
-    reading = sensor_temp.read_u16() * conversion_factor 
-    temperature = 27 - (reading - 0.706)/0.001721
-    print("The current temperature is: {}".format(temperature))
-    utime.sleep(5)
+while True: 
+    reading = sensor_temp.read_u16() * conversion_factor #read and convert the temperature 
+    temperature = 27 - (reading - 0.706)/0.001721 #read and convert the temperature 
+    print("The current temperature is: {}".format(temperature)) #print the temperature
+    utime.sleep(5) #sleep for 5 seconds
