ca96e8fc1c20caf4b10297c677f3587a467ebe37
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-02-02 13:18:19 +0200

    Add files via upload

---
+++ MicroPython/Digital Output/Digital_Output_LED_ADV.py
@@ -0,0 +1,12 @@
+from machine import Pin, Timer
+led = Pin(2, Pin.OUT)
+timer = Timer()
+
+def blink(timer):
+    led.toggle() #tobble the LED on/off
+
+timer.init(freq=1, mode=Timer.PERIODIC, callback=blink) #call the blink function and blink infinitely!
+
+# with freq the frequency (time) can be adjusted
+# periodic timer stays active infinitely by default for more information about the timer
+# library you can check out: https://blog.logrocket.com/understanding-flutter-timer-class-timer-periodic/
