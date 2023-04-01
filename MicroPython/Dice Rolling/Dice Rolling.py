0c9678278043a99c1dcac0a7cb0009bb7ac93e5e
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-04-01 17:46:56 +0300

    Add files via upload

---
+++ MicroPython/Dice Rolling/Dice Rolling.py
@@ -0,0 +1,12 @@
+# By Alexandros Panagiotakopoulos
+
+from machine import Pin, I2C
+import random
+import utime as time
+
+cnt=1 #create a counter to print dice rolls
+while True:
+    dice1 = random.randint(1, 6)
+    print("You have rolled the dice:",cnt,"times")
+    print("The result was:",dice1)
+    time.sleep(10)
